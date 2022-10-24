3b7bfe2dab054ee7236d50e485823d5e41614f36
Kevin Leister <kevinleister07@gmail.com>
2022-10-24 14:08:17 -0500

    initial commit

---
+++ src/components/Detail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Detail = (props) => {
+  return (
+
+          <p className='info'>{props.info}</p>
+
+  );
+}
+
+export default Detail
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+//3. Use props to render the default Beyonce contact card
+//so the Card component can be reused for other contacts.
+//4. Import the contacts.js file to create card components.
